12824b467485a546131d1f6273fa2ddc683678c1
Or Huba <hubaor00@gmail.com>
2025-06-22 18:15:02 +0300

    "adding flexbox layout & a bunch of domain classes"

---
+++ app/src/main/java/com/example/tasksapp/assistantApp/core/utils/DatesHandle.java
@@ -0,0 +1,9 @@
+package com.example.tasksapp.assistantApp.core.utils;
+
+public final class DatesHandle {
+    public final static String DEFUALT_DATE_FORMAT = "dd:mm:yyyy";
+    private DatesHandle(){
+        // empty useless constructor
+    }
+
+}
---
+++ app/src/main/java/com/example/tasksapp/assistantApp/core/utils/Utils.java
@@ -0,0 +1,6 @@
+package com.example.tasksapp.assistantApp.core.utils;
+
+public class Utils {
+    public  enum Priority {High, Medium, Low}
+
+}
