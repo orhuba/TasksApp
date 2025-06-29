4e9cbf54482ee270486f803916a078d6b4c5da77
Or Huba <hubaor00@gmail.com>
2025-06-29 18:39:16 +0300

    layout setup

--- app/src/main/java/com/example/tasksapp/MainActivity.java
+++ app/src/main/java/com/example/tasksapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tasksapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ListView listView = findViewById(R.id.list);
+        //int resource = android.R.layout.two_line_list_item;
+        String[] tasks = {"Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6", "Task 7", "Task 8", "Task 9", "Task 10", "Task 11", "Task 12"
+        , "Task 13", "Task 14", "Task 15", "Task 16", "Task 17", "Task 18", "Task 19", "Task 20", "Task 21", "Task 22", "Task 23", "Task 24", "Task 25"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_checked,tasks);
+        listView.setAdapter(adapter);
+
     }
 }
